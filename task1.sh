1aa115c8234b00dbbd32cb6eb38fb82b7ec655d9
rcmcphee <rcmcphee34@gmail.com>
2025-02-08 15:30:47 -0500

    Working Task 1

--- task1.sh
+++ task1.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-chmod +x infinite.sh
-
 # Run infinite
 sh infinite.sh
 
-# Get process ID (PID)
-PID=$(ps -ef | grep "/usr/bin/sh" | grep -v "grep" | awk '{print $2}')
-
 # Kill the infinite process via its PID
-kill -9 $PID
+kill -9 $(ps -ef | grep "/usr/bin/sh" | grep -v "grep" | awk '{print $2}')

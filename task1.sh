90af5e79ec0cc57ef002e559d64395e7b9ce445c
rcmcphee <rcmcphee34@gmail.com>
2025-02-08 14:45:49 -0500

    Initial commit

---
+++ task1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Run infinite script
+sh infinite.sh
+
+
+your script  # run ehere to see what your script produces
+ 
+kill -9 $(ps | ...) # run the same script here to see what it produces
